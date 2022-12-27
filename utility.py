6e709a6995e30b02ab3550752ac64a70fc8c8097
segovelo <gauchosinpingo@gmail.com>
2022-12-27 23:36:11 +0000

    Refactoring of utility method

--- utility.py
+++ utility.py
@@ -7,18 +7,39 @@ def check_row(rows):
             return row[0]
     return "n"
 
-def check_column(columns):
-    for column in columns:
-        if column[0]==column[1] and column[1]==column[2]:
-            return column[0]
+def check_column(rows):
+    for j in range(0,len(rows[0])):
+        if rows[0][j]==rows[1][j] and rows[1][j]==rows[2][j]:
+            return rows[0][j]
     return "n"
 
-states=[[["x","o","o"],["o","x","o"],["x","o","o"]],[["x","o","x"],["x","o","o"],["o","x","o"]],[["x","o","o"],["o","x","o"],["o","x","x"]]]
+def check_diagonal(rows):
+    i = 0
+    winner_found=False
+    for j in range(0,len(rows[0])-1):
+        if rows[j][j]==rows[j+1][j+1]:
+            winner_found=True
+        else:
+            winner_found=False
+            break
+    if winner_found: return rows[0][0]
+    winner_found=False
+    for j in range(len(rows[0])-1, 0, -1):
+        if rows[len(rows[0])-j-1][j]==rows[len(rows[0])-j][j-1]:
+            winner_found=True
+        else:
+            winner_found=False
+            break
+    if winner_found: return rows[0][len(rows[0])-1]
+    return "n"
+
+
+states=[[["x","o","o"],["o","x","o"],["x","o","o"]],[["x","o","x"],["x","o","o"],["o","x","o"]],[["x","o","o"],["x","x","x"],["o","x","o"]],[["x","x","o"],["o","o","x"],["o","x","x"]],[["x","o","o"],["o","x","o"],["o","x","x"]]]
 winner_found = False
-for state in states and not winner_found:
+for state in states:
     winner = check_row(state)
     if winner == "n":
-        columns = [[]]
-        for row in state:
-            columns[row.index].append(row)
-        winner = check_column(columns)
+        winner = check_column(state)
+    if winner == "n":
+        winner = check_diagonal(state)
+    print ("winner is : ", winner)
